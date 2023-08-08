492504ebe25d996dfc52c79a4e73b02d62b901fa
kirstenval03 <kirs0307@gmail.com>
2023-08-08 13:50:58 -0600

    DELETE BUTTON WORKING

--- middleware/isItemOwner.js
+++ middleware/isItemOwner.js
@@ -1,19 +1,24 @@
 const Item = require('../models/Item');
 
-const isItemOwner = (req, res, next) => {
 
-    Item.findById(req.params.id)
+const isItemOwner = (req, res, next) => {
+    const { itemId } = req.params;
+
+    Item.findById(itemId)
         .then((foundItem) => {
-            if (req.user._id === foundItem.owner.toString()) {
-                next()
+            console.log("req.user:", req.user);
+            console.log("foundItem:", foundItem);
+
+            if (foundItem.owner._id.toString() === req.user._id.toString()) {
+                next();
             } else {
-                res.status(401).json({message: "Validation Error"})
+                res.status(403).json({ message: "Access denied. You are not the owner of this item." });
             }
         })
         .catch((err) => {
-            console.log(err)
-            next(err)
-        })
-}
+            console.log(err);
+            next(err);
+        });
+};
 
-module.exports = isItemOwner
+module.exports = isItemOwner;
